cbe0dc993f10ca1b0b6fa026ede880b7f5bb0eeb
Tim Wilson <hello@smarttypes.org>
2014-11-27 07:49:03 -0500

    More scaffold tweaking, close to being able to start testing.

--- graph.py
+++ graph.py
@@ -1,30 +1,29 @@
 
 import graphlab as gl
+import relaxmap
 
-class Graph(object):
+class GraphWrapper(object):
 
     def __init__(self, vertices, edges, parent=None):
         self.parent = parent
-        self.vertices = vertices
-        self.edges = edges
-        self.sgraph = gl.SGraph(vertices=self.vertices, edges=self.edges, 
+        self.g = gl.SGraph(vertices=vertices, edges=edges, 
             vid_field='v_id', src_field='src_id', dst_field='dst_id')
 
     def homes_for_the_homeless(self):
         """
-        Assign a community to nodes that need one
+        Randomly assign a community to nodes that need one
         """
 
-    def pagerank_em(self):
+    def do_pagerank(self):
         """"""
 
-    def set_community_vertices(self):
+    def get_community_vertices(self):
         """
-        groupby self.sgraph['community_id']
+        groupby self.g['community_id']
         """
         self.community_vertices = gl.SFrame()
 
-    def set_community_edges(self):
+    def get_community_edges(self):
         """
         join g.edges w/ g.vertices to get SFrame like:
          - src_id, dst_id, src_community_id, dst_community_id
@@ -33,28 +32,30 @@ class Graph(object):
         """
         self.community_edges = gl.SFrame()
 
-    def get_community_g(self):
-        return Graph(self.community_vertices, self.community_edges, parent=self)
-
-    def find_communities(self):
-        "return list of Graphs"
-        if self.parent:
-            #pull subgraph out of self.parent.g
-            #partition_community_ids = self.parent.g['v_id']
-            #partition_g = self.sgraph[self.sgraph['community_id'] in _partition_community_ids]
-
-    def partition_and_update_communities(self):
+    def get_community_gw(self):
         self.homes_for_the_homeless()
-        self.set_community_vertices()
-        self.set_community_edges()
-        community_g = self.get_community_g()
-        total_mdl = 0
-        for partition_g, partition_mdl in community_g.find_communities():
-            for partition_community_g, partition_community_mdl in partition_g.find_communities():
-                total_mdl += partition_community_mdl
-                "update self.sgraph.vertices, set community_id"
-                "update self.sgraph.community_vertices, set new community_ids and partition_ids"
-        return total_mdl
+        return Graph(self.get_community_vertices(), 
+            self.get_community_edges(), parent=self)
+
+    def find_communities(self, partitions=None):
+        if not partitions:
+            communities, mdl = relaxmap.find_communities(self.g)
+            #update self.g.vertices w/ community info
+        else:
+            mdl = 0
+            for partition in partitions:
+                communities, _mdl = relaxmap.find_communities(partition)
+                mdl += _mdl
+                #update self.g.vertices w/ community info
+
+        if self.parent:
+            partition_graphs = []
+            for partition in communities:
+                #pull subgraph out of self.parent.g
+                partition_graphs.append(partition_graph)
+            return self.parent.find_communities(partitions=partition_graphs)
+        return mdl
 
     def save(self):
         """"""
+        self.vertices
--- run.py
+++ run.py
@@ -4,13 +4,14 @@ import sys
 
 vertices = graphlab.get_vertices(sys.args[1])
 edges = graphlab.get_edges(sys.args[2])
-g = Graph(vertices, edges)
+gw = GraphWrapper(vertices, edges)
 
 progress_threshold = 1000 #this will take some tweaking
 progress = progress_threshold + 1
 last_mdl = 1e20
 while progress > progress_threshold:
-    mdl = g.partition_and_update_communities()
+    community_gw = gw.get_community_gw()
+    mdl = community_gw.find_communities()
     print mdl
     progress = last_mdl - mdl
     last_mdl = mdl
